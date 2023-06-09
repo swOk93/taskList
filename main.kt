ab4f008cbabec736c7d820c5530a42c0c22cdf72
swOk93 <91178260+swOk93@users.noreply.github.com>
2023-06-09 14:33:20 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,9 +1,23 @@
 package tasklist
+import kotlinx.datetime.*
 
+fun checkDateTime(): Boolean {
+
+}
 object TaskList {
-    val taskList = mutableListOf<MutableList<String>>()
+    private val taskList = mutableListOf<MutableList<String>>()
     fun add() {
         val task = mutableListOf<String>()
+        var priority = ""
+        var date: String
+        var time: String
+        while (priority.isEmpty()) {
+            println("Input the task priority (C, H, N, L):")
+            readln().uppercase().let { if ((it == "C") or (it == "H") or (it == "N") or (it == "L")) priority = it }
+        }
+        println("Input the date (yyyy-mm-dd):")
+        while (checkDateTime())
+        println("Input the time (hh:mm):")
         println("Input a new task (enter a blank line to end):")
         while (true) {
             readln().trim().also { if (it == "") {
@@ -37,3 +51,5 @@ fun main() {
         }
     }
 }
+
+
